a9026b9db4ae5c0c82c5fe487e2352a71c6b63dd
Eduard <eduardwille1@gmail.com>
2021-10-28 11:41:39 +0200

    styling before update, use as fallback(ignore commit)

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -1,18 +1,18 @@
 @include('includes.header')
 @include('includes.navbar') 
 
-<div class="container"> 
+<div class="container mt-2"> 
   @if(isset($category)) 
     <div class='container text-center'>
-      <h1 class='display-3' style='text-transform: capitalize; color: white; text-shadow: 0px 0px 3px black;'>{{ $category }}</h1>
+      <h1 class='display-3' style='text-transform: capitalize;'>{{ $category }}</h1>
     </div>
   @else
     <div class='container text-center'>
-      <h1 class='display-3' style='color: white; text-shadow: 0px 0px 3px black;'>All products</h1>
+      <h1 class='display-3'>All products</h1>
     </div>
   @endif
 
-  <div style="display: flex; flex-wrap: wrap;">
+  <div style="display: flex; flex-wrap: wrap; max-height: 80vh; overflow: scroll;">
   @if (isset($allProducts))
   @foreach ($allProducts as $index)
   <div class="card text-center mt-3" style="width: 18rem; float: left; margin-left: 1rem;">
